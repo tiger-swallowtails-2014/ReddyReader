2bfa248c510357b6bee4b3714ac12bdc36c099e9
Tali Herzka <taliherzka@gmail.com>
2014-09-26 12:00:15 -0700

    fixed spec file

--- spec/features/speed_test_spec.rb
+++ spec/features/speed_test_spec.rb
@@ -1,20 +1,10 @@
 feature "speed test" do
 	scenario "start button is displayed" do
 		visit '/speed_test'
-<<<<<<< HEAD
-		expect(page).to have_content "Start"
-	
-	end
-
-=======
 
 		# click_button 'Start'
 
 		expect(page).to have_content 'Start'
 	end
 
-
-
-
->>>>>>> a4cda455bcf361358f0d22e017d08b86cd1ea378
 end
