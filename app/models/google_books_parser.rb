84fb55f5b4ecb8919c093625b8e25fa6da701b20
Jayrobin <james.michael.robinson@googlemail.com>
2014-09-25 19:53:09 -0700

    Add Google Books Parser

---
+++ app/models/google_books_parser.rb
@@ -0,0 +1,27 @@
+require 'uri'
+require 'httparty'
+require 'json'
+
+class GoogleBooksParser
+  def self.get_books(search_term)
+    json_response = HTTParty.get(build_search_url(search_term))
+    json_response.parsed_response["items"].map do |item|
+      parseBookFromJSON(item["volumeInfo"])
+    end
+  end
+
+  private
+    def self.parseBookFromJSON(item_json)
+      book_data = {}
+      book_data[:author] = item_json["authors"].join(", ")
+      book_data[:title] =  item_json["title"]
+      book_data[:page_count] = item_json["pageCount"]
+      book_data[:image_url] = item_json["imageLinks"]["thumbnail"]
+
+      Book.new(book_data)
+    end
+
+    def self.build_search_url(search_term)
+      URI.escape("https://www.googleapis.com/books/v1/volumes?q=#{search_term}")
+    end
+end
