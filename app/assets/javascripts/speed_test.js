1f77cdf68c731f18f9de1a8d6391852c10096044
Tali Herzka <taliherzka@gmail.com>
2014-09-25 17:54:41 -0700

    added basic timer to speed_test.js

--- app/assets/javascripts/speed_test.js
+++ app/assets/javascripts/speed_test.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-	$('#start').click(function(){		
+	$('#start').click(function(event){
 		$('#testarea').show();
+		var start = event.timeStamp;
+		console.log(start);
 		$(this).hide();
 		$('#done').show();
+		$('#done').click(function(event){
+			var diff = event.timeStamp - start;
+			$('#testparagraph').append("<p>Your time was: " + diff/1000 + " seconds </p>");
+		});
 	});
 
 
 });
+
+var speedTimer = function(event){
+
+}
