c773340c16a6422ae7d5a72ea8e3689f62086c45
Kyle Howell <mrkylehowell@gmail.com>
2014-09-25 17:36:10 -0700

    add search bar for index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+
+  post 'search_results' => 'static_pages#test'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
