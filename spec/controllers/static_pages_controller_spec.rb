05f7429e38670488f00e755d99770b201ee8cbc6
DevReddy <RyanEddyDev@gmail.com>
2014-09-25 17:32:07 -0700

    Create static_pages_controller spec file with empty routes.

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe StaticPagesController do
+  context "#index" do
+
+  end
+
+  context "#search_results" do
+
+  end
+
+  context "#speed_test" do
+
+  end
+
+  context "#speed_results" do
+
+  end
+end
