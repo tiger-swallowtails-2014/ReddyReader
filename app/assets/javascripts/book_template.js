1183a11d9ccd26415bb867f87a4f0705b79d5c4e
Jayrobin <james.michael.robinson@googlemail.com>
2014-09-27 14:24:42 -0700

    Add thumbnail styling to book images

--- app/assets/javascripts/book_template.js
+++ app/assets/javascripts/book_template.js
@@ -1,5 +1,5 @@
 var bookTemplate =  "<div class='book col-sm-3'>" +
-                      "<img src='{{image_url}}' alt='{{title}} front cover'>" +
+                      "<img src='{{image_url}}' alt='{{title}} front cover' class='img-thumbnail'>" +
                       "<p class='title'>{{title}}</p>" +
                       "<p class='author'>{{author}}</p>" +
                       "<p style='display:none;' class='page_count'>{{page_count}}</p>" +
