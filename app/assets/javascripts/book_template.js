0931125fa2aee7e7c3f33f7877fee50807b63c7c
Jayrobin <james.michael.robinson@googlemail.com>
2014-09-27 14:07:56 -0700

    Reformat bookTemplate.js

--- app/assets/javascripts/book_template.js
+++ app/assets/javascripts/book_template.js
@@ -1,1 +1,6 @@
-var bookTemplate = "<div class='book'><img src='{{image_url}}'><p class='title'>{{title}}</p><p class='author'>{{author}}</p><p style='display:none;' class='page_count'>{{page_count}}</p></div><br>"
+var bookTemplate =  "<div class='book'>" +
+                      "<img src='{{image_url}}'>" +
+                      "<p class='title'>{{title}}</p>" +
+                      "<p class='author'>{{author}}</p>" +
+                      "<p style='display:none;' class='page_count'>{{page_count}}</p>" +
+                    "</div>"
