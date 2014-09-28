276493ebbe584f7ae337b3c04ecb468a41f06a36
Tali Herzka <taliherzka@gmail.com>
2014-09-27 18:34:29 -0700

    random styling changes

--- app/assets/javascripts/book_template.js
+++ app/assets/javascripts/book_template.js
@@ -1,4 +1,4 @@
-var bookTemplate =  "<div class='book col-sm-3'>" +
+var bookTemplate =  "<div class='book col-sm-2'>" +
                       "<img src='{{image_url}}' alt='{{title}} front cover' class='img-thumbnail'>" +
                       "<p class='title'>{{title}}</p>" +
                       "<p class='author'>{{author}}</p>" +
