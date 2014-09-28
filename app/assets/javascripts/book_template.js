e8cc2198cefe6758607975b12fb926f2a9f73060
Tali Herzka <taliherzka@gmail.com>
2014-09-28 15:02:45 -0700

    changed glyph extension to .svg, centered carousel content and moved indicators

--- app/assets/javascripts/book_template.js
+++ app/assets/javascripts/book_template.js
@@ -1,5 +1,5 @@
-var bookTemplate =  "<div class='book col-sm-2'>" +
+var bookTemplate =  "<div class='book col-xs-3 text-center'>" +
                       "<img src='{{image_url}}' alt='{{title}} front cover' class='img-thumbnail'>" +
-                      "<p class='title'>{{title}}</p>" +
+                      "<div class='row'><p class='title'>{{title}}</p>" +
                       "<p class='author'>{{author}}</p>" +
-                      "<p style='display:none;' class='page_count'>{{page_count}}</p>" + "</div>"
+                      "<p style='display:none;' class='page_count'>{{page_count}}</p></div>" + "</div>"
