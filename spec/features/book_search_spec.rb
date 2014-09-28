228ccb571e75ff6b848d0fc1cf6b4b3b1ec58044
Kyle Howell <mrkylehowell@gmail.com>
2014-09-27 17:44:44 -0700

    book search feature testing

--- spec/features/book_search_spec.rb
+++ spec/features/book_search_spec.rb
@@ -9,8 +9,27 @@ describe "Book search feature" do
   it "returns a list of matching books when I perform a search" do
     visit root_path
     fill_in 'searchfield', with: "1984"
+    wait_for_ajax
+    expect(page).to have_css(".book")
+  end
+
+  it "allows me to search again when no matching books are returned" do
+    visit root_path
+    fill_in 'searchfield', with: "!@#$%"
+    wait_for_ajax
+    expect(page).to_not have_css(".book")
+    fill_in 'searchfield', with: "1984"
+    wait_for_ajax
+    expect(page).to have_css(".book")
+  end
+
+  it "allows me to search again when the matching books aren't what I'm looking for" do
+    visit root_path
+    fill_in 'searchfield', with: "harry potter"
+    wait_for_ajax
+    expect(page).to_not have_content "George Orwell"
+    fill_in 'searchfield', with: "1984"
+    wait_for_ajax
     expect(page).to have_content "George Orwell"
   end
-  it "allows me to search again when no matching books are returned"
-  it "allows me to search again when the matching books aren't what I'm looking for"
 end
