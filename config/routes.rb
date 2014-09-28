828949c8cd661b143c16233c423fa86689d8d66b
Tali Herzka <taliherzka@gmail.com>
2014-09-28 02:03:22 -0700

    created algorithm and mustache template to display books at random along with how long the current user will take to read them

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'search_results' => 'static_pages#search_results', as: :search_results
   get 'speed_test' => 'static_pages#speed_test', as: :speed_test
   post 'speed_test_result' => 'static_pages#speed_test_result', as: :speed_test_result
-  get '/login_success' => 'static_pages#login_success'
+ 
+  get 'random_book_display' => 'static_pages#random_book_display', as: :random_book_display
   
 end
