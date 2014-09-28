2489fc9dce26eb9531dd9ad814ff83e62354b47b
Kyle Howell <mrkylehowell@gmail.com>
2014-09-27 17:03:13 -0700

    test needs ajax helper method

--- spec/features/book_search_spec.rb
+++ spec/features/book_search_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 describe "Book search feature" do
-  it "returns a list of matching books when I perform a search"
+  it "renders the welcome message" do
+    visit root_path
+    expect(page).to have_content "What would you like to read?"
+  end
+
+  it "returns a list of matching books when I perform a search" do
+    visit root_path
+    fill_in 'searchfield', with: "1984"
+    expect(page).to have_content "George Orwell"
+  end
   it "allows me to search again when no matching books are returned"
   it "allows me to search again when the matching books aren't what I'm looking for"
 end
