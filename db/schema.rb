8b59e84e7fa18786510a013b128710ed2a01c171
Tali Herzka <taliherzka@gmail.com>
2014-09-29 00:15:39 -0700

    logged in user partial fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140928222458) do
+ActiveRecord::Schema.define(version: 20140929043441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20140928222458) do
     t.string   "isbn"
     t.integer  "page_count",     default: 0, null: false
     t.integer  "est_word_count", default: 0
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -44,10 +43,6 @@ ActiveRecord::Schema.define(version: 20140928222458) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "paragraphs", force: true do |t|
-    t.text "test"
   end
 
 end
