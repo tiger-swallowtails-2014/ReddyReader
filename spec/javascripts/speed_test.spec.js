f42c34b69e8c8bcdf6123869891131b730be354c
Kyle Howell <mrkylehowell@gmail.com>
2014-09-29 23:03:58 -0700

    check start timer

--- spec/javascripts/speed_test.spec.js
+++ spec/javascripts/speed_test.spec.js
@@ -1,18 +1,22 @@
 describe("Speed Test specs", function(){
   beforeEach(function(){
-    speedTest = new ReddyReader.SpeedTest();
+    // speedTest = new ReddyReader.SpeedTest();
   });
 
   it("recieves a paragraph to display from the server", function(){
+    var speedTest = new ReddyReader.SpeedTest();
 
   });
 
   it("starts a timer",function(){
-    startTime = speedTest.startTimer;
-    expect(startTime).toEqual(new Date().getTime())
+    var speedTest = new ReddyReader.SpeedTest();
+    speedTest.startTimer();
+    expect(speedTest.startTime).toEqual(new Date().getTime())
   });
 
   it("gets elapsed time once timer is stopped",function(){
+    var speedTest = new ReddyReader.SpeedTest();
+    speedTest.startTimer();
 
   });
 
@@ -23,4 +27,4 @@ describe("Speed Test specs", function(){
   it("recieves speed test results back from server", function(){
 
   });
-}
+})
