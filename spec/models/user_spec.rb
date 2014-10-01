fb3fc55515cdaf729ca4c8afb828126ed5c90e63
Kyle Howell <mrkylehowell@gmail.com>
2014-10-01 16:32:04 -0700

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,15 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe User do
+  context "validations" do
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :password_digest }
+    it { should validate_uniqueness_of :username }
+  end
+
+  context "associations" do
+    it { should have_many :reading_tests }
+  end
+end
