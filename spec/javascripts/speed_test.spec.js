a2492d8fc3360370ba456f5aed9ea906e31bbfc8
Kyle Howell <mrkylehowell@gmail.com>
2014-09-30 21:37:23 -0700

    create bind method for test enviornment

--- spec/javascripts/speed_test.spec.js
+++ spec/javascripts/speed_test.spec.js
@@ -1,16 +1,34 @@
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+    return fBound;
+  };
+}
+
 describe("Speed Test specs", function(){
   beforeEach(function(){
     speedTest = new ReddyReader.SpeedTest();
   });
 
-  // it("recieves a paragraph to display from the server", function(){
-  //   var speedTest = new ReddyReader.SpeedTest();
+  it("recieves a paragraph to display from the server", function(){
 
-  // });
+  });
 
   it("starts a timer",function() {
     spyOn(Date.prototype, "getTime").and.returnValue(12345)
-
     var speedTest = new ReddyReader.SpeedTest();
     speedTest.startTimer();
     expect(speedTest.startTime).toEqual(12345);
@@ -18,7 +36,6 @@ describe("Speed Test specs", function(){
 
   it("stops a timer",function(){
     spyOn(Date.prototype, "getTime").and.returnValue(5)
-
     var speedTest = new ReddyReader.SpeedTest();
     spyOn(speedTest, "sendResults");
     speedTest.startTime = 3
@@ -28,23 +45,16 @@ describe("Speed Test specs", function(){
 
   it("sends elapsed time and word count to server",function(){
     var speedTest = new ReddyReader.SpeedTest();
-
-    // promise = { done: function() {} }
     promise = jasmine.createSpyObj("promise", ["done"]);
-    // spyOn(promise, "done")
     spyOn(speedTest.server, "ajax").and.returnValue(promise);
     spyOn(speedTest.handleServerResponse, "bind").and.returnValue("some-function");
-
     speedTest.sendResults(12345);
-
     expect(speedTest.server.ajax).toHaveBeenCalledWith({
       url: "/speed_test_result",
       method: "post",
       data: {"time": 12345}
     })
-
     expect(speedTest.handleServerResponse.bind).toHaveBeenCalledWith(speedTest);
-
     expect(promise.done).toHaveBeenCalledWith("some-function");
   });
 })
