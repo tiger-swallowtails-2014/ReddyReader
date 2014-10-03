447b24286cb6ed84bfa9a9a64b6cf6b99e0a13f1
Jayrobin <james.michael.robinson@googlemail.com>
2014-10-02 19:56:24 -0700

    final styling changes for larger screen

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -19,7 +19,7 @@ class StaticPagesController < ApplicationController
   def speed_test_result
     reading_test = ReadingTest.create( time_elapsed: params[:time], paragraph_id: session[:paragraph_id], book_id: recent_book.id)
     current_user.reading_tests << reading_test if current_user && params[:give_user_wpm]
-    render json: {author: recent_book.author, image_url: recent_book.image_url, wpm: reading_test.wpm, result: reading_test.time_to_read, title: recent_book.title, time_per_page: reading_test.time_per_page}.to_json
+    render json: {author: recent_book.author, image_url: recent_book.image_url, wpm: reading_test.wpm, result: reading_test.time_to_read, title: recent_book.title, time_per_page: reading_test.time_per_page, page_count: recent_book.page_count}.to_json
   end
 
   def random_book_display
@@ -51,7 +51,7 @@ class StaticPagesController < ApplicationController
       render json: {books: [], time_per_page: 0}.to_json
     end
   end
-  
+
   def all_gr_books
     if current_user && current_user.from_goodreads? && current_user.recent_wpm
       all_books = HTTParty.get('https://www.goodreads.com/review/list?format=xml&v=2', :query => {:key => ENV["GR_API_KEY"], :id => current_user.uid, :sort => 'date_updated', :page => 1-3})["GoodreadsResponse"]["reviews"]["review"]
@@ -64,7 +64,7 @@ class StaticPagesController < ApplicationController
       render json: {books: [], time_per_page: 0}.to_json
     end
   end
-  
+
   def skip_speed_test
     test = current_user.reading_tests.last
     session[:paragraph_id] = test.paragraph.id
